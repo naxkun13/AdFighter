5acc37acf85b6a4c1780bdc24448d97fae637c38
Arberg <22754697@mail.ru>
2019-01-07 01:27:03 +0400

    States
    Добавил мобам состояния, чтобы было легче ими оперировать в дальнейшем.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : NPC {
+
+	[SerializeField]
+	private CanvasGroup healthGroup;
+
+	private Transform playerPos;
+
+    private IState currentState;
+
+    public float MyAttackRange { get; set; }
+
+	public Transform Target
+	{
+		get
+		{
+			return playerPos;
+		}
+
+		set
+		{
+			playerPos = value;
+		}
+	}
+
+    protected void Awake()
+    {
+        MyAttackRange = 1;
+        ChangeState(new IdleState());
+    }
+
+	protected override void Update ()
+	{
+        currentState.Update();
+
+		base.Update ();
+	}
+  
+
+    public void ChangeState(IState newState)
+    {
+        if (currentState != null)
+        {
+            currentState.Exit();
+        }
+
+        currentState = newState;
+
+        currentState.Enter(this);
+    }
+
+		
+}
