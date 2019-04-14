2ef361f0d9276549e04cef66a285e87ba0b1a7e5
Anton Evstifeev <a.s.evstifeev@gmail.com>
2019-04-14 22:54:14 +0300

    Добавил джойстики
    1) Поместил все UI элементы в канвас
    2) Добавил два джойстика: движение и направление
    3) Написал новый скрипт, который обрабатывает движение и направленеи стрельбы по виртуальным осям (eidit - project settings - Input - Axes) - кнопкам или по джойстикам.

---
+++ Assets/Scripts/Protagonist/ProtagonistMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class ProtagonistMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 250.0f;
+    [SerializeField] private Joystick joystickMovement;
+    [SerializeField] private Joystick joystickDirection;
+
+    private Rigidbody2D _body;
+    private Animator _anim;
+
+    void Start() {
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+    
+    void Update() {
+        float deltaSpeed = speed * Time.deltaTime;                  // Убираем влияние частоты обновления кадров
+        float deltaY = Input.GetAxis("Vertical")   != .0 ? Input.GetAxis("Vertical")   : joystickMovement.Vertical;
+        float deltaX = Input.GetAxis("Horizontal") != .0 ? Input.GetAxis("Horizontal") : joystickMovement.Horizontal;
+        Vector2 movement = new Vector2(deltaX, deltaY) * deltaSpeed;
+        _body.velocity = movement;
+
+        _anim.SetFloat("speed", Mathf.Abs(deltaX) + Mathf.Abs(deltaY));
+
+        float direction = Input.GetAxis("Fire") != .0 ? Input.GetAxis("Fire") : joystickDirection.Horizontal;
+
+        if (!Mathf.Approximately(direction, 0)) {
+            transform.localScale = new Vector3(Mathf.Sign(direction) * 0.5f, 0.5f, 1);
+        }
+    }
+}
